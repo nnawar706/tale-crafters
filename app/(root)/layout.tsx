6f7979da898b2ef244d01b87b3fbac17bf087800
nnawar706 <nnawar706@gmail.com>
2024-06-04 17:48:29 +0600

    sidebar-left

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,7 @@
+import MobileNav from "@/components/MobileNav";
 import SidebarLeft from "@/components/SidebarLeft";
+import SidebarRight from "@/components/SidebarRight";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -6,10 +9,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main>
-        <SidebarLeft/>
-        {children}
-        right bar
+    <main className="relative flex flex-col">
+        <section className="relative flex bg-black-3">
+            <SidebarLeft/>
+            
+            <div className="flex flex-1 flex-col min-h-screen p-4 sm:p-14">
+                <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+                    <div className="flex h-16 items-center justify-between md:hidden">
+                        <Image src={"/icons/logo.svg"} alt="logo" width={30} height={30}/>
+                        
+                        <MobileNav/>
+                    </div>
+                    <div className="flex flex-col md:pb-14">
+                        /* toast */
+                        {children}
+                    </div>
+                </div>
+            </div>
+
+            <SidebarRight/>
+        </section>
     </main>
   );
 }
--- components/SidebarRight.tsx
+++ components/SidebarRight.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const SidebarRight = () => {
   return (
-    <div>SidebarRight</div>
+    <section className="sticky right-0 top-0 flex 
+    w-[310px] flex-col overflow-y-hidden border-none 
+    bg-black-1 px-[30px] pt-8 max-xl:hidden"></section>
   )
 }
 
