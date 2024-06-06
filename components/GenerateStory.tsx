413d315ca44628e5ed57abb44a9199b8559c6040
nnawar706 <nnawar706@gmail.com>
2024-06-07 02:52:23 +0600

    generate story

--- components/GenerateStory.tsx
+++ components/GenerateStory.tsx
@@ -1,5 +1,7 @@
 import { GenerateStoryProps } from '@/types'
-import React from 'react'
+import React, { useState } from 'react'
+import { useToast } from './ui/use-toast'
+import { Textarea } from './ui/textarea'
 
 const GenerateStory = ({ 
     setAudioStorageId, 
@@ -9,10 +11,19 @@ const GenerateStory = ({
     voicePrompt, 
     setVoicePrompt, 
     setAudioDuration }: GenerateStoryProps) => {
+        const [generating, isGenerating] = useState(false)
+        const { toast } = useToast()
     return (
-        <div>
-        
-        </div>
+        <>
+            <div className="flex flex-col gap-2">
+                <p className="text-14 font-bold text-white-1">Generate Storyline</p>
+                <Textarea
+                    className="input-class font-light focus-visible:ring-offset-orange-1"
+                    placeholder="Provide Text to Generate Audio"
+                    rows={5}
+                />
+            </div>
+        </>
     )
 }
 
